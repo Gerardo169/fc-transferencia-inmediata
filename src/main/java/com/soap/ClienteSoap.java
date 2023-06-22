a7d7a72eab26a6cd97137c102f4729f063f30009
KevinG <diazsosa16@gmail.com>
2023-06-22 15:28:14 -0500

    commit

---
+++ src/main/java/com/soap/ClienteSoap.java
@@ -0,0 +1,8 @@
+package com.soap;
+
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+
+public class ClienteSoap extends WebServiceGatewaySupport {
+	
+
+}
---
+++ src/main/java/org/fmbbva/movcli/fc/transferencia/inmediata/FcTransferenciaInmediataApplication.java
@@ -0,0 +1,13 @@
+package org.fmbbva.movcli.fc.transferencia.inmediata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcTransferenciaInmediataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcTransferenciaInmediataApplication.class, args);
+	}
+
+}
